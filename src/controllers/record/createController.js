af2751406609d4eb838490a11ae4f9e5dbac3759
Gustavo <rodrigues.gustavo2@aluno.ifsp.edu.br>
2024-11-07 17:47:44 -0300

    img upload começado

--- src/controllers/record/createController.js
+++ src/controllers/record/createController.js
@@ -1,4 +1,17 @@
 import { create } from "../../models/recordModel.js"
+import multer from 'multer';
+import express from 'express'; 
+import cloudinary from 'cloudinary';
+
+const router = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+cloudinary.v2.config({
+    cloud_name: 'dfcgapbcr',
+    api_key: '297652483131342',
+    api_secret: 'A8SkZ4QWtBZQZHBzO54Dg2azI0I'
+});
 
 const createController = async (req, res, next) => {
     try{
@@ -18,5 +31,21 @@ const createController = async (req, res, next) => {
         next(error)
     }
 }
+
+try {
+    const uploadOptions = {
+        resource_type: 'auto',
+        public_id: `exam/${Date.now()}_${req.file.originalname}`, 
+    };
+
+    const uploadResponse = await new Promise((resolve, reject) => {
+        cloudinary.v2.uploader.upload_stream(uploadOptions, (error, result) => {
+            if (error) {
+                return reject(new Error('Erro ao fazer upload da imagem.'));
+            }
+            resolve(result);
+        }).end(req.file.buffer);
+    });
+
 
 export default createController
