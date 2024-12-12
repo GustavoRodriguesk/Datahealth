328cbb7d7e3582b1b7db6240e6f4431187592f33
GustavoRodriguesk <161630558+GustavoRodriguesk@users.noreply.github.com>
2024-12-12 13:51:07 -0300

    trash removal

--- src/routers/doctorRouter.js
+++ src/routers/doctorRouter.js
@@ -2,14 +2,12 @@ import express from 'express'
 import createDoctorController from '../controllers/doctor/createDoctor.js'
 import updateDoctorController from '../controllers/doctor/updateDoctor.js'
 import deleteDoctorController from '../controllers/doctor/deleteDoctor.js'
-// import getByIdDoctorController from '../controllers/doctor/GetbyIdDoctor.js'
 import listDoctorController from '../controllers/doctor/listDoctor.js'
 const router = express.Router()
 
 router.post('/', createDoctorController)
 router.put('/:id', updateDoctorController)
 router.delete('/:id', deleteDoctorController)
-// router.get('/:id', getByIdDoctorController)
 router.get('/list', listDoctorController)
 
 export default router
